a45293a5e999e752ecaa81fb63fe1e8141fb212a
Keanu73 <guestone63@gmail.com>
2019-05-18 14:17:15 +0100

    Revert back to SQL for links

--- index.js
+++ index.js
@@ -44,10 +44,17 @@ client.api = require("./api.js")
 // Add investment calculating crap and whatever in
 client.math = require("./math.js")
 
-// Storing Discord + Reddit links
-// They are more of a "favourite" option rather than 
-// a strict OAuth2 verification link
-client.links = new Enmap({ name: "links" })
+// Add MySQL database for storing Discord + Reddit links
+client.pool = mysql.createPool({
+	host: client.config.mysql.host,
+	port: client.config.mysql.port,
+	user: client.config.mysql.user,
+	password: client.config.mysql.password,
+	database: client.config.mysql.database,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+})
 
 // We're doing real fancy node 8 async/await stuff here, and to do that
 // we need to wrap stuff in an anonymous function. It's annoying but it works.
