9a13435b97ff3c54b5c8a8744e9574975cf732c0
Keanu73 <guestone63@gmail.com>
2019-05-21 20:42:38 +0100

    Remember to actually test shit before you push, kids

--- commands/stats.js
+++ commands/stats.js
@@ -2,13 +2,11 @@ const { RichEmbed } = require("discord.js")
 const moment = require("moment")
 exports.run = async (client, message, [name], _level) => {
 	const check = await client.api.getLink(message.author.id)
-	const username = check ? check : name
+	const username = check ? check : name.replace(/^((\/|)u\/)/g, "")
 
 	if (!name && !check) return message.reply(":question: Please supply a Reddit username.")
 
 	if (name.length < 3 && !check) return message.reply(":thinking: Something tells me that is not a Reddit username")
-
-	if (name.startsWith("u/") || name.startsWith("/u/")) name.replace(/^((\/|)u\/)/g, "")
 
 	const user = await client.api.getInvestorProfile(username).catch(err => client.logger.error(err.stack))
 	if (user.id === 0) return message.reply(":question: I couldn't find that user. Sorry")
